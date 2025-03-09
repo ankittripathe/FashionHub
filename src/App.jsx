7ee97623ae16660b8b024b7f78f8f52a7cdbd342
Dhoni <ankitdhonee@gmail.com>
2025-03-09 16:47:30 +0545

    added browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouer, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouer>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="men" />} />
+          <Route path="/womens" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kid" />} />
+        </Routes>
+      </BrowserRouer>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
