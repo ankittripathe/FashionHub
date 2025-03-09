afd19df216057174eadc7b0e5d3d73455b04e47a
Dhoni <ankitdhonee@gmail.com>
2025-03-09 17:08:28 +0545

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouer, Routes, Route } from "react-router-dom";
 import Shop from "./Pages/Shop";
 import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
 
 const App = () => {
   return (
@@ -15,6 +18,13 @@ const App = () => {
           <Route path="/mens" element={<ShopCategory category="men" />} />
           <Route path="/womens" element={<ShopCategory category="women" />} />
           <Route path="/kids" element={<ShopCategory category="kid" />} />
+
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
       </BrowserRouer>
     </div>
