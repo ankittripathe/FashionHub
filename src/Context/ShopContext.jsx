2c5a07ebbef0c89bead4f9481e103324d3cc0a7c
Dhoni <ankitdhonee@gmail.com>
2025-03-11 15:25:05 +0545

    shopcontext

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,18 @@
+import React, { createContext } from "react";
+import all_product from "../Components/Assets/all_product";
+
+// create context
+const ShopContextCreated = createContext(null);
+
+const ShopContext = (props) => {
+  const contextValue = { all_product };
+
+  return (
+    <ShopContextCreated.Provider value={contextValue}>
+      {props.children}
+    </ShopContextCreated.Provider>
+  );
+};
+
+export default ShopContext;
+export { ShopContextCreated };
