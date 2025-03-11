5ad6b7781cc7e51f019e64ded66ea63c64078111
Dhoni <ankitdhonee@gmail.com>
2025-03-11 15:39:11 +0545

    shop category

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './Styles/ShopCategory.css'
+import { ShopContextCreated } from '../Context/ShopContext'
 
 const ShopCategory = () => {
+  const {all_product} = useContext(ShopContextCreated)
   return (
-    <div>
+    <div className='shop-category'>
       
     </div>
   )
