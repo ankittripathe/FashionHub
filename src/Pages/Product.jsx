0f0962c90bec69c55de4be2984ace1cb223d8cc6
Dhoni <ankitdhonee@gmail.com>
2025-03-12 16:37:14 +0545

    changes

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContextCreated } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
 
 const Product = () => {
+  const { all_product } = useContext(ShopContextCreated);
+  const { productId } = useParams();
+  // find and match
+  const product = all_product.find((e) => e.id === Number(productId));
   return (
     <div>
-      
+      <Breadcrum product={product} />
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
