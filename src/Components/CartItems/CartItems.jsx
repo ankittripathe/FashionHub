c797738773cc27531a9d7665f43a6924d2d5c35c
Dhoni <ankitdhonee@gmail.com>
2025-03-13 11:40:32 +0545

    cartItems

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import './CartItems.css'
+import React from "react";
+import "./CartItems.css";
+import { useContext } from "react";
+import { ShopContextCreated } from "../../Context/ShopContext";
+import remove_icon from '../Assets/cart_cross_icon.png'
 
 const CartItems = () => {
-  return (
-    <div className='cartItems'>
-      
-    </div>
-  )
-}
+  const { all_product, cartItems, addToCart, removeFromCart } =
+    useContext(ShopContextCreated);
 
-export default CartItems
+  return (
+    <div className="cartItems">
+      <div className="cartItems-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+      <div>
+        <div className="cartItem-format">
+          <img src="" alt="" className="carticon-product-icon" />
+          <p></p>
+          <p></p>
+          <button className="cartItems-quantity"></button>
+          <p>
+
+          </p>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItems;
